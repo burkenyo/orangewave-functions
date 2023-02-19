8d964f9d0a7ed9e442eaa36d573498764a7a7fcf
Sammo Gabay <sammo@sammo.ga>
2023-02-19 00:23:03 -0500

    Generate ionos-dns-api with plain fetch and add scaffolding guardrail

---
+++ scaffold.js
@@ -0,0 +1,17 @@
+import { existsSync } from "node:fs";
+import { exit } from "node:process";
+import OpenAPI from "openapi-typescript-codegen";
+
+if (existsSync("./src/ionos-dns-api") && !process.argv.includes("--force")) {
+  console.log("Scaffolded API already generated and has changes that would be overwritten. "
+    + "Use --force to override.");
+
+  exit(1);
+}
+
+OpenAPI.generate({
+  input: "ionos-dns-api.yaml",
+  output: "src/ionos-dns-api/",
+  postfix: "Client",
+  indent: OpenAPI.Indent.SPACE_2
+});
